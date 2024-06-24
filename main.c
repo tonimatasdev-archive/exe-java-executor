55db22074c76dfee21e1e8960352802236b8c2e9
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-06-24 13:25:34 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(void) {
+    STARTUPINFO startupInfo;
+    PROCESS_INFORMATION processInformation;
+
+    ZeroMemory(&startupInfo, sizeof(startupInfo));
+    startupInfo.cb = sizeof(startupInfo);
+    ZeroMemory(&processInformation, sizeof(processInformation));
+
+    const char command[] = "java -jar neutron-1.0.0.jar";
+
+    if (!CreateProcess(NULL,command, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &startupInfo, &processInformation)) {
+        printf("CreateProcess falló (%lu).\n", GetLastError());
+        return 1;
+    }
+
+    SetForegroundWindow(processInformation.hProcess);
+
+    WaitForSingleObject(processInformation.hProcess, INFINITE);
+
+    CloseHandle(processInformation.hProcess);
+    CloseHandle(processInformation.hThread);
+
+    return 0;
+}
